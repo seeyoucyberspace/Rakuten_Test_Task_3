d2fbbb0dd7214db3467cfde16ebf40d2797ba421
Serge <73642062+seeyoucyberspace@users.noreply.github.com>
2024-11-07 08:14:09 +0200

    alfa version of third test

--- ui_tests/specs/sweetshop-tests/basket.spec.js
+++ ui_tests/specs/sweetshop-tests/basket.spec.js
@@ -1,6 +1,7 @@
 import { productSteps } from '../../steps/productSteps';
 import { basketPageObject } from '../../page_objects/BasketPage';
 import * as credentials from '../../../credentials/credentials.json';
+import { productActions } from "../../actions/productActions";
 
 describe('Sweet Shop UI Tests', () => {
     beforeEach(() => {
@@ -9,22 +10,36 @@ describe('Sweet Shop UI Tests', () => {
 
         // Add products to the basket
         productSteps.addMultipleProductsToBasket();
+        basketPageObject.navigateToBasket();
     });
 
     it('Verify all selected items are present in basket', () => {
-        // Navigate to the basket page
-        basketPageObject.navigateToBasket();
-
         // Verify that the basket contains 4 items
         productSteps.verifyBasketItemCount(4);
     });
 
     it('Verify total price in basket matches individual item prices', () => {
-        // Navigate to the basket page
-        basketPageObject.navigateToBasket();
-
         // Verify the total price matches the sum of the individual item prices
         productSteps.verifyBasketTotalPrice();
+    });
+
+    it('Verify that selecting Standard Shipping updates the total price', () => {
+        const previousTotalPrice = productActions.getBasketTotalPrice();
+        // Click on the Standard Shipping delivery option
+        cy.get('label[for="exampleRadios2"]').click();
+
+        // Verify the total price changes after selecting Standard Shipping
+        const updatedTotalPrice = productActions.getBasketTotalPrice();
+        cy.wait(500); // Adding some delay for price update
+        productActions.getBasketTotalPrice().then((updatedTotalPrice) => {
+            if (isNaN(updatedTotalPrice)) {
+                throw new Error('Price value is NaN after updating delivery option');
+            }
+            if (updatedTotalPrice === previousTotalPrice) {
+                throw new Error('Price did not change after updating delivery option');
+            }
+            expect(updatedTotalPrice).to.not.equal(previousTotalPrice);
+        });
     });
 
     // Other tests...
